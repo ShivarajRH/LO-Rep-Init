95aacd975ecc6cc5efc48525a8fcb7e9248aac7c
Shivaraj RH <publish@lyfeon.com>
2013-10-27 19:20:32 -1200

    dbtest file moved from footer

---
+++ assets/dbtest.php
@@ -0,0 +1,12 @@
+<?php 
+include '/includes/db_connection.php';
+
+$rslt=mysql_query("show tables",$linkid);
+if(mysql_errno()) {
+    print mysql_error($linkid);
+}
+$rslt_arr = mysql_affected_rows($linkid);
+
+//echo $rslt_arr."";
+
+?>
--- footer_reg.php
+++ footer_reg.php
@@ -19,14 +19,3 @@
 			</li>
 		</ul>
 	</div>
-<?php include 'includes/db_connection.php';
-
-$rslt=mysql_query("show tables",$linkid);
-if(mysql_errno()) {
-    print mysql_error($linkid);
-}
-$rslt_arr = mysql_affected_rows($linkid);
-
-echo $rslt_arr."";
-
-?>
