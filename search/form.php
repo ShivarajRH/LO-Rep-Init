4735daef97887e70f81c24700361109893c2c6b3
Shivaraj RH <publish@lyfeon.com>
2013-12-27 09:54:32 -1200

    Dec_27_2013 Updates-Search api Object oreiented conversion

--- search/form.php
+++ search/form.php
@@ -14,10 +14,16 @@ if(isset($_POST) ) {
     $lat = 112;
     $long = 76;
     $src = 'stream';
-    
+
     $s = microtime(true);
     //request the api
     $post = array('requesting_uid'=>  urlencode($uid),'query'=>urlencode($query_str),"timestamp"=>urlencode($timestamp),'lat'=>urlencode($lat),'long'=>urlencode($long),'src'=>urlencode($src));
+    
+    $get=$site_url.'api/search/?action_object=search_content&requesting_uid='.urlencode($uid).'&query='
+        .urlencode($query_str).'&timestamp='.urlencode($timestamp).'&lat='.urlencode($lat).'&long='
+        .urlencode($long).'&src='.urlencode($src).'';
+    
+//    echo '<pre>';print_r($get);die();
     $url = $site_url.'api/search/?action_object=search_content';
     $result = $ob->getApiContent($url, 'json',$post);
     echo 'Success:<br>';
